ebae3ec9ece6513dbd2959f3577855af0a683115
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-23 17:36:22 +0800

    [Fix] Retrieve various collections from db

--- preprocess.py
+++ preprocess.py
@@ -3,20 +3,21 @@ import pandas as pd
 
 class DataPreprocess(object):
 
-    def __init__(self, nrow=None):
-        self.nrow = nrow
+    def __init__(self):
+        pass
 
     def _load_db(self):
         client = MongoClient('localhost', 27017)
         db = client['CreditRisk']
-        if self.nrow:
-            cursor = db.user_info.find().limit(self.nrow)
-        else:
-            cursor = db.user_info.find().limit(1000)
-        data = pd.DataFrame(list(cursor))
-        data = data.drop('_id', axis=1)
+        cursor = db.user_bank.find()
+        user_bank = pd.DataFrame(list(cursor))
+        user_bank.drop("_id", axis=1, inplace=True)
+        cursor = db.user_bill.find()
+        user_bill = pd.DataFrame(list(cursor))
+        user_bill.drop("_id", axis=1, inplace=True)
+        cursor = db.overdue.find()
+        overdue = pd.DataFrame(list(cursor))
+        overdue.drop("_id", axis=1, inplace=True)
         client.close()
-        return data
-
-
+        return user_bank, user_bill, overdue
 
