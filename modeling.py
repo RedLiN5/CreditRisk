28e0132b57da59af76cf7af3ac8c433814c785b3
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-30 10:52:12 +0800

    [Feature] Bernoulli Naive Bayes

--- modeling.py
+++ modeling.py
@@ -1,9 +1,21 @@
 from preprocess import DataPreprocess
-
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
 
 class Model(object):
 
     def __init__(self):
-        pass
+        dp = DataPreprocess()
+        df = dp.run()
+        X = df.drop('Overdue', axis=1)
+        y = df['Overdue']
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+            train_test_split(X, y, test_size=0.3, random_state=1)
 
     # TODO Bernoulli_NB, XGB, RandomForest, Adaboost
+    def _bernoulli_NB(self):
+        clf = BernoulliNB()
+        clf.fit(self.X_train, self.y_train)
+        score = clf.score(self.X_test, self.y_test)
+        print('Accuracy rate of Naive Bayes: {0:.3f}'.format(score))
+    
